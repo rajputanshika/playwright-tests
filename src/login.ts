99bf8f7ddfd7f9859bc6cf04c63e15e9d82d56af
Anshika rajput <rajputanshika526@gmail.com>
2026-07-03 16:59:01 +0530

    updated test cases

--- src/login.ts
+++ src/login.ts
@@ -4,23 +4,61 @@ export class LoginPage {
     readonly page: Page;
     readonly orgname: Locator;
     readonly saveButton: Locator;
+    readonly clickonLink: Locator;
+    readonly email: Locator;
+    readonly password: Locator;
+    readonly signInButton: Locator;
 
     constructor(Browserpage: Page) {
         this.page = Browserpage;
-        this.orgname = Browserpage.locator('//input[@name="orgname"]');
+        this.orgname = Browserpage.locator('//input[@name="company"]');
         this.saveButton = Browserpage.locator('//button[@type="submit"]');
+        this.clickonLink = Browserpage.locator('//h5[@id="asset-infinity-login-button"]');
+        this.email = Browserpage.locator('#loginForm input[name="email"]');
+        this.password = Browserpage.locator('#loginForm input[name="password"]');
+        this.signInButton = Browserpage.locator('#loginForm button[type="submit"]');
 
+        
     }
 
-    async navigateToLoginPage() {
-        await this.page.goto('https://app-uat.assetinfinity.io/Login');
-    }
+async navigateToLoginPage() {
+    await this.page.goto('https://app-uat.assetinfinity.io/Login', {
+        waitUntil: 'commit',
+        timeout: 60000
+    });
+
+    // Wait for actual login input instead of page load
+    await this.page.locator('//input[@name="company"]').waitFor({
+        state: 'visible',
+        timeout: 60000
+    });
+}
+
+
 
     async enterOrgName(value: string) {
         await this.orgname.fill(value);
     }
 
+
     async clickSaveButton() {
         await this.saveButton.click();
+    }
+
+  async clickLink() {
+        await this.clickonLink.click();
+    }
+
+
+     async fillEmail(value: string) {
+        await this.email.fill(value);
+    }
+
+     async fillPassword(value: string) {
+        await this.password.fill(value);
+    }
+
+     async signIn() {
+        await this.signInButton.click();
     }
 }
--- tests/logintest.spec.ts
+++ tests/logintest.spec.ts
@@ -5,7 +5,10 @@ test('Login test', async ({ page }) => {//start with this
     const loginPage = new LoginPage(page);//create an object
 //The constructor runs automatically when use 'new' from login.ts
     await loginPage.navigateToLoginPage();
-    await page.waitForTimeout(5000);
     await loginPage.enterOrgName('UAT');
     await loginPage.clickSaveButton();
+    await loginPage.clickLink();
+    await loginPage.fillEmail('support@pcsinfinity.in');
+    await loginPage.fillPassword('abc');
+    await loginPage.signIn();
 });
